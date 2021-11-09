ca5f9be5f846dbf1be64b35a9c55191d2cd35b83
SuellenBergold <93959504+SuellenBergold@users.noreply.github.com>
2021-11-09 20:10:33 -0300

    Add files via upload

--- src/components/cursos/adict.js
+++ src/components/cursos/adict.js
@@ -2,25 +2,38 @@
 import React from "react";
 // Importando os components necessários da lib react-materialize
 import { Row, Col, Card, Input, Button } from 'react-materialize';
+import api from "../../service/api"
 
-const Adicionar = () => (
+const Adicionar = () => {
+
+const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+
+const cad = async () =>{
+		await api.post("", {nome: name, descricao: description});
+
+return (
   <Row>
     <Col m={12} s={12}>
         <h5>Cursos</h5>
         <Card>
           <Row>
               <Input placeholder="Digite aqui..." 
-              label="Nome" s={12} />
+              label="Nome" s={12} 
+		onChange={event => setName(event.target.value)}/>
 
               <Input placeholder="Digite aqui..." 
-              label="Descrição" s={12} />
+              label="Descrição" s={12} 
+		onChange={event => setDescription(event.target.value)}/>
+
             <Col s={12} m={12}>
-              <Button waves='light' className="right grey darken-2">Adicionar</Button>
+              <NavLink to="/cursos" ><Button onClick={cad} waves='light' className="right grey darken-2">Adicionar</Button></NavLink>
             </Col>
           </Row>
         </Card>
     </Col>
   </Row>
 );
+}
 
 export default Adicionar
